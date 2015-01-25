c37857559abca702c70e4178c4381747583b43c2
Varakhedi Sujeet <svarakhedi@pivotal.io>
2015-01-24 19:17:12 -0800

    use start.rb recipe

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,7 +1,8 @@
 include_recipe "opentsdb::install_opentsdb"
 include_recipe "opentsdb::install_tcollector"
+include_recipe "opentsdb::start"
 
-cookbook_file "/home/vagrant/start_all.sh" do
-  source "start_all.sh"
-  mode 0755
-end
+# cookbook_file "/home/vagrant/start_all.sh" do
+#   source "start_all.sh"
+#   mode 0755
+# end
